7943440a5f31ef909ec1afcf1468dbd9cb073c59
beckattack <romeo.beck@yahoo.com>
2019-06-15 12:03:20 +0200

    Test

---
+++ MovieCritics01.py
@@ -0,0 +1,15 @@
+import numpy as np
+import re
+import os
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from plot_confusion_matrix import *
+
+
+#test
+#test
